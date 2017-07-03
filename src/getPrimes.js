d47a80d4baab196625fde1fad6bc195b1c068674
Obum <obumanichebe@gmail.com>
2017-07-03 19:10:38 +0100

    a functioning getprimes function

--- src/getPrimes.js
+++ src/getPrimes.js
@@ -1,30 +1,32 @@
-
-
-var result = [];
 function getPrime(n){
-  if (n<0){
+  var result = [];
+  if (n<=1){
     return "Input must be a number"
-  }
+  }//end of if statement
+
   else{
-for(var i=2; i<=n; i++){
-            let isPrime = true;
+
+      for(var i=2; i<=n; i++){
+            var isPrime = true;
           
-          for(var j = 2; j <= (Math.sqrt(i)); j++){
+        for(var j = 2; j <= (Math.sqrt(i)); j++){
             
-             if(i%j===0){
+             if(i%j==0){
                  isPrime = false;
                      } //end of if statement
-        
-            }//end of inner for loop
-    
-            if(isPrime===true){
+
+          }//end of inner for loop
+
+            if(isPrime==true){
                 result.push(i);
                     }//end of if statement
-  
-}//end of for loop
-  
-}// end of else
-return result;
-}
+
+          }//end of for loop
+
+      return result;
+
+    }// end of else
+
+}//end of function getprime
 
 module.exports = getPrime
--- test/test.js
+++ test/test.js
@@ -5,45 +5,45 @@ describe("getprimes", function(){
 
 	describe("handle valid input", function(){
 		it ("should return [2,3,5] as primes for 5",function(){
-			assert.equal(myprimes(6), [2,3,5]);
+			assert.deepEqual(myprimes(5), [2,3,5]);
 		});//end of it block
 
-		it ("should return [2,3,5,7] as primes for 5",function(){
-			assert.equal(myprimes(8), [2,3,5,7]);
+		it ("should return [2,3,5,7] as primes for 8",function(){
+			assert.deepEqual(myprimes(8), [2,3,5,7]);
 		});//end of it block
 
-		it ("should return [2,3,5,7,11] as primes for 5",function(){
-			assert.equal(myprimes(12), [2,3,5,7,11]);
+		it ("should return [2,3,5,7,11] as primes for 12",function(){
+			assert.deepEqual(myprimes(12), [2,3,5,7,11]);
 		});//end of it block
 
 		it ("should return [2,3,5,7,11,13] as primes for 5",function(){
-			assert.equal(myprimes(14), [2,3,5,7,11,13]);
+			assert.deepEqual(myprimes(14), [2,3,5,7,11,13]);
 		});//end of it block
 
 		it ("should return [2,3,5,7,11,13,17] as primes for 5",function(){
-			assert.equal(myprimes(18), [2,3,5,7,11,13,17]);
+			assert.deepEqual(myprimes(18), [2,3,5,7,11,13,17]);
 		});//end of it block
 
 		it ("should return [2,3,5,7,11,13,17,19] as primes for 5",function(){
-			assert.equal(myprimes(20), [2,3,5,7,11,13,17,19]);
+			assert.deepEqual(myprimes(20), [2,3,5,7,11,13,17,19]);
 		});//end of it block
 
 		it ("should return undefined as primes for -5",function(){
-			assert.equal(myprimes(-5), "negative numbers are not primes");
+			assert.deepEqual(myprimes(-5), "Input must be a number");
 		});//end of it block
 
 		it ("should return not a prime number primes for 0",function(){
-			assert.equal(myprimes(0), "not a prime number");
+			assert.deepEqual(myprimes(0), "Input must be a number");
 		});//end of it block
 
 		it ("should return not a prime number ",function(){
-			assert.equal(myprimes(1), "not a prime number");
+			assert.deepEqual(myprimes(1), "Input must be a number");
 		});//end of it block	
 
 		it ("should return [2,3,5,7,11,13,17,19] as primes for 25",function(){
-			assert.equal(myprimes(25), [2,3,5,7,11,13,17,19,23]);
+			assert.deepEqual(myprimes(25), [2,3,5,7,11,13,17,19,23]);
 		});//end of it bloc
 
 		});//end of inner describe block
 
-	}); //end of describe
+	}); //end of describe 
